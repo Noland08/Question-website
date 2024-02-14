2fe0afc2c48181c278a0ce0c05d7843f05c6e082
Noland08 <144099927+Noland08@users.noreply.github.com>
2024-02-14 23:52:22 +0700

    Add files via upload

---
+++ backend.js
@@ -0,0 +1,22 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-bth");
+const noBth = document.querySelector(".no-bth");
+
+yesBtn.addEventListener("click", () => {
+    question.innerHTML="awww ur Gay ";
+});
+
+noBth.addEventListener("click", () => {
+    const noBtnRect = noBth.getBoundingClientRect();
+    const maxX = window.innerWidth - noBtnRect.width;
+    const maxY = window.innerHeight - noBtnRect.height;
+
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+    noBth.style.left = Math.floor((Math.random() * 250) + 1) + "px";
+    noBth.style.top = Math.floor((Math.random() * 200) + 1) + "px";
+});
+
+// console.log("hello world")
